c7d54181b5f78ed25f633580a4b7b2d4dd2782a0
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-08 10:45:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+let num1, num2;
+num1 = 31;
+num2 = 2;
+let multiply;
+
+if (num1 * num2 === 62) {
+   multiply = 62; 
+}
+
+console.log(multiply);
+
+let random=Math.floor(Math.random()*10)
+console.log(random);
+
+
+let num3, num4;
+num3 = 24;
+num4 = 5;
+let mod;
+if(num3%num4 === 4){
+    mod = num3%num4;
+}
+
+let max = Math.max (2,3,4,5,6,6,6,6,13,20,19,3,4,5);
+console.log(max)
+
+let numbers = (2,3,4,5,6,6,6,6,13,20,19,3,4,5);
+let myMax;
+if (Math.max(numbers) === 20) {
+    myMax = Math.max(numbers);
+}
+
+console.log(myMax)
